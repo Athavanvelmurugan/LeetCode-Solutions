d560459722cea8d2d32be6f7827dfe02a1ae6591
Athavan V <athavanvelmurugan@gmail.com>
2025-09-18 12:15:52 +0530

    Create MergeSortList.java

---
+++ src/LinkedList/MergeSortList.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+      ListNode result = new ListNode(0);
+      ListNode ptr = result;
+
+      while(list1!=null && list2!=null){
+        if(list1.val<=list2.val){
+            ptr.next=list1;
+            list1=list1.next;
+        }else{
+            ptr.next=list2;
+            list2=list2.next;
+        }
+        ptr=ptr.next;
+      }
+      if(list1!=null){
+        ptr.next=list1;
+      }
+      if(list2!=null){
+        ptr.next=list2;
+      }
+
+      return result.next;  
+    }
+}
