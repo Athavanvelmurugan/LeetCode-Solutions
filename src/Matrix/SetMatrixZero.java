9b2278b1a46504cee75f296b6345243e021f8ebe
Athavan V <athavanvelmurugan@gmail.com>
2025-09-25 07:01:25 +0530

    Create SetMatrixZero.java

---
+++ src/Matrix/SetMatrixZero.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       boolean firstrow =false; boolean firstcol =false;
+       int n =matrix.length; int m =matrix[0].length;
+       for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrix[i][j]==0){
+                if(i==0)firstrow=true;
+                if(j==0)firstcol=true;
+                matrix[0][j]=0;
+                matrix[i][0]=0;
+            }
+        }
+       }
+
+       for(int i=1; i<n; i++){
+        for(int j=1; j<m; j++){
+            if(matrix[0][j]==0 || matrix[i][0]==0){
+                matrix[i][j]=0;
+            }
+        }
+       }
+
+       if(firstrow){
+        for(int i=0; i<m; i++){
+            matrix[0][i]=0;
+        }
+       } 
+
+       if(firstcol){
+        for(int j=0; j<n; j++){
+            matrix[j][0]=0;
+        }
+       }
+    }
+}
