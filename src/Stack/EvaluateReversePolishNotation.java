bac5efa01c5b0ea17e7ce89d152597efe76197df
Athavan V <athavanvelmurugan@gmail.com>
2025-09-29 20:08:08 +0530

    Create EvaluateReversePolishNotation.java

---
+++ src/Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer>stack = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+")){
+                stack.push(stack.pop()+stack.pop());
+            }else if(token.equals("-")){
+                stack.push(-stack.pop()+stack.pop());
+            }else if(token.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }else if(token.equals("/")){
+                int num =stack.pop();
+                stack.push(stack.pop()/num);
+            }else{
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.peek();
+    }
+}
