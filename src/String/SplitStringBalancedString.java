2e8653b7215a6a0bfb216f1f8ede8c69b29dc7d8
Athavan V <athavanvelmurugan@gmail.com>
2025-10-13 17:30:13 +0530

    Create SplitStringBalancedString.java

---
+++ src/String/SplitStringBalancedString.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int left =0; int right=0; int count=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='R'){
+                left++;
+            }else{
+                right++;
+            }
+            if(left==right){
+                count++;
+            }
+        }
+        return count;
+    }
+}
