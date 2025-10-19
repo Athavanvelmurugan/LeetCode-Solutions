9aaf42c1ec6e4be84c7026937e55c0cffe65f917
Athavan V <athavanvelmurugan@gmail.com>
2025-10-19 19:20:53 +0530

    Create SpiralMatrix.java

---
+++ src/Matrix/SpiralMatrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+       List<Integer> list = new ArrayList<>();
+       int rowbegin=0; int rowend =matrix.length-1;
+       int colbegin=0; int colend=matrix[0].length-1;
+
+       while(rowbegin<=rowend && colbegin<=colend){
+        
+        for(int i=colbegin; i<=colend;i++){
+            list.add(matrix[rowbegin][i]);
+        }
+
+        rowbegin++;
+
+        for(int i=rowbegin; i<=rowend; i++){
+            list.add(matrix[i][colend]);
+        }
+
+        colend--;
+
+        if(rowbegin<=rowend){
+            for(int i=colend; i>=colbegin; i--){
+                list.add(matrix[rowend][i]);
+            }
+            rowend--;
+        }
+        if(colbegin<=colend){
+            for(int i=rowend; i>=rowbegin; i--){
+                list.add(matrix[i][colbegin]);
+            }
+            colbegin++;
+        }
+       } 
+       return list;
+    }
+}
